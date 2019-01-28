26d32ab281eb1cfc1c06dac8a71d6b1ce32e00cc
Dean Camera <dean@fourwalledcubicle.com>
2019-01-28 14:10:21 +1100

    Use custom key image and text for the example exit button.

--- src/example.py
+++ src/example.py
@@ -12,20 +12,15 @@ import threading
 from PIL import Image, ImageDraw, ImageFont
 
 
-# Custom tiles with run-time generated text and custom image via the PIL module
-def get_key_image(deck, key, state):
-    # Get the required key image dimensions
-    image_format = deck.key_image_format()
-    width = image_format['width']
-    height = image_format['height']
-    order = image_format['order']
-
+# Generates a custom tile with run-time generated text and custom image via the
+# PIL module.
+def render_key_image(width, height, rgb_order, icon_filename, label_text):
     # Create new key image of the correct dimensions, black background
     image = Image.new("RGB", (width, height), 'black')
 
     # Add image overlay, rescaling the image asset if it is too large to fit the
     # requested dimensions via a high quality Lanczos scaling algorithm
-    icon = Image.open("Assets/{}.png".format("Pressed" if state else "Released")).convert("RGBA")
+    icon = Image.open(icon_filename).convert("RGBA")
     icon.thumbnail((width, height - 20), Image.LANCZOS)
     image.paste(icon, (0, 0), icon)
 
@@ -33,7 +28,7 @@ def get_key_image(deck, key, state):
     # number onto the image
     font = ImageFont.truetype("Assets/Roboto-Regular.ttf", 14)
     draw = ImageDraw.Draw(image)
-    draw.text((10, height - 20), text="Key {}".format(key), font=font, fill=(255, 255, 255, 128))
+    draw.text((10, height - 20), text=label_text, font=font, fill=(255, 255, 255, 128))
 
     # Get the raw r, g and b components of the generated image (note we need to
     # flip it horizontally to match the format the StreamDeck expects)
@@ -42,17 +37,65 @@ def get_key_image(deck, key, state):
     # Recombine the B, G and R elements in the order the display expects them,
     # and convert the resulting image to a sequence of bytes
     rgb = {"R": r, "G": g, "B": b}
-    return Image.merge("RGB", (rgb[order[0]], rgb[order[1]], rgb[order[2]])).tobytes()
+    return Image.merge("RGB", (rgb[rgb_order[0]], rgb[rgb_order[1]], rgb[rgb_order[2]])).tobytes()
 
 
+# Returns styling information for a key based on its position and state.
+def get_key_style(deck, key, state):
+    # Last button in the example application is the exit button
+    exit_key_index = deck.key_count() - 1
+
+    if key == exit_key_index:
+        name = "exit"
+        icon = "Assets/Exit.png"
+        text = "Exit"
+    else:
+        name = "emoji"
+        icon = "Assets/{}.png".format("Pressed" if state else "Released")
+        text = "Pressed!" if state else "Key {}".format(key)
+
+    return {"name": name, "icon": icon, "label": text}
+
+
+# Creates a new key image based on the key index, style and current key state
+# and updates the image on the StreamDeck.
+def update_key_image(deck, key, state):
+    # Get the required key image dimensions
+    image_format = deck.key_image_format()
+    width = image_format['width']
+    height = image_format['height']
+    rgb_order = image_format['order']
+
+    # Determine what icon and label to use on the generated key
+    style = get_key_style(deck, key, state)
+
+    # Generate the custom key with the requested image and label
+    image = render_key_image(width, height, rgb_order, style["icon"], style["label"])
+
+    # Update requested key with the generated image
+    deck.set_key_image(key, image)
+
+
+# Prints key state change information, updates rhe key image and performs any
+# associated actions when a key is pressed.
 def key_change_callback(deck, key, state):
+    # Print new key state
     print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
 
-    deck.set_key_image(key, get_key_image(deck, key, state))
+    # Update the key image based on the new key state
+    update_key_image(deck, key, state)
 
-    if key == deck.key_count() - 1:
-        deck.reset()
-        deck.close()
+    # Check if the key is changing to the pressed state
+    if state:
+        key_style_name = get_key_style(deck, key, state)["name"]
+
+        # When an exit button is pressed, close the application
+        if key_style_name == "exit" and state:
+            # Reset deck, clearing all button images
+            deck.reset()
+
+            # Close deck handle, terminating internal worker threads
+            deck.close()
 
 
 if __name__ == "__main__":
@@ -65,15 +108,17 @@ if __name__ == "__main__":
         deck.open()
         deck.reset()
 
-        print("Press key {} to exit.".format(deck.key_count() - 1), flush=True)
-
         deck.set_brightness(30)
 
+        # Set initial key images
         for key in range(deck.key_count()):
-            deck.set_key_image(key, get_key_image(deck, key, False))
+            update_key_image(deck, key, False)
 
+        # Register callback function for when a key state changes
         deck.set_key_callback(key_change_callback)
 
+        # Wait until all application threads have terminated (for this example,
+        # this is when all deck handles are closed)
         for t in threading.enumerate():
             if t is threading.currentThread():
                 continue
