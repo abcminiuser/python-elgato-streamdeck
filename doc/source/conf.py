fcbb3cea1d2728c4baef152ca4cdc0588aec9700
Dean Camera <dean@fourwalledcubicle.com>
2019-02-21 21:46:52 +1100

    Use m2r module to render README.md in sphinx docs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -96,8 +96,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# so a file named "default.css" will overwrite the built-in "default.css".
+html_static_path = []
+
+html_extra_path = ['../../ExampleDeck.jpg']
 
 html_extra_path = ['../../ExampleDeck.jpg']
 
