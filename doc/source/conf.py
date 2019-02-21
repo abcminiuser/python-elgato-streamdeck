ffa62268303a0a39e3714caa335dde64061bf590
Dean Camera <dean@fourwalledcubicle.com>
2019-02-21 21:46:52 +1100

    Use m2r module to render README.md in sphinx docs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'm2r']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -98,6 +98,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_extra_path = ['../../ExampleDeck.jpg']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
