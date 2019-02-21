946f749610b56a29cf2b4a70c1d23a16d8fe74d3
Dean Camera <dean@fourwalledcubicle.com>
2019-02-21 19:22:01 +1100

    Add Python packaging files.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+   name='streamdeck',
+   version='0.2',
+   description='Library to control Elgato StreamDeck devices.',
+   author='Dean Camera',
+   author_email='dean@fourwalledcubicle.com',
+   url='https://github.com/abcminiuser/python-elgato-streamdeck',
+   package_dir={'': 'src'},
+   packages=setuptools.find_namespace_packages(where='src'),
+   install_requires=['hidapi'],
+   license="MIT",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   include_package_data=True,
+)
