887b0bdd8eae1ef0f2f0eeb65d9a91357e34402e
Dean Camera <dean@fourwalledcubicle.com>
2019-02-23 11:37:22 +1100

    Fix Sphinx import path.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ import sys
 
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../../src/StreamDeck'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- General configuration ------------------------------------------------
