68399f7dc61fd6d88038e08a46b1d85156ac2f2a
Dean Camera <dean@fourwalledcubicle.com>
2020-05-17 11:18:25 +1000

    Set minimum Sphinx version in documentation config.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(ROOT_PATH, 'src')))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
