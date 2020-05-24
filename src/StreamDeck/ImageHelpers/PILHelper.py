c61d11d23fe1a8d1f725794a638d30ae72fb70a6
Jack Mudge <jmudge14@gmail.com>
2020-05-23 23:19:32 -0700

    Added PILHelper.load_scaled_image()

--- src/StreamDeck/ImageHelpers/PILHelper.py
+++ src/StreamDeck/ImageHelpers/PILHelper.py
@@ -29,6 +29,28 @@ def create_image(deck, background='black'):
 
     return Image.new("RGB", image_format['size'], background)
 
+def load_scaled_image(deck, image_path, background='black'):
+    """
+    Loads an image from image_path and scales it to fit the given StreamDeck
+    device's keys, and centers it.
+
+    .. seealso:: See :func:`~PILHelper.to_native_format` method for converting a
+                 PIL image instance to the native image format of a given
+                 StreamDeck device
+
+    :rtrype: PIL.Image
+    :return: Loaded PIL image scaled and centered
+    """
+    image = PILHelper.create_image(deck, background=background)
+
+    # Resize the source image asset to best-fit the dimensions of a single key,
+    # and paste it onto our blank frame centered as closely as possible.
+    icon = Image.open(image_path).convert("RGBA")
+    icon.thumbnail((image.width, image.height), Image.LANCZOS)
+    icon_pos = ((image.width - icon.width) // 2, (image.height - icon.height) // 2)
+    image.paste(icon, icon_pos, icon)
+
+    return image
 
 def to_native_format(deck, image):
     """
