10d1836be975305c1e599ea5ae5799036ed64db0
Jack Mudge <jmudge14@gmail.com>
2020-05-23 23:48:15 -0700

    Satisfy Flake8 style guide - remove PIL.Image import from example_basic.py

--- src/example_basic.py
+++ src/example_basic.py
@@ -13,7 +13,7 @@
 import os
 import threading
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
 
