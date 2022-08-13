a3a377b35fba48369a11dc397f7f8d1d9d3daba6
Dean Camera <dean@fourwalledcubicle.com>
2022-08-13 16:16:01 +1000

    Remove redundant method documentation from subclasses of StreamDeck, Sphinx will use the base class documentation.

--- src/StreamDeck/Devices/StreamDeckMini.py
+++ src/StreamDeck/Devices/StreamDeckMini.py
@@ -42,14 +42,6 @@ class StreamDeckMini(StreamDeck):
     ] + [0] * (KEY_PIXEL_WIDTH * KEY_PIXEL_HEIGHT * 3)
 
     def _read_key_states(self):
-        """
-        Reads the key states of the StreamDeck. This is used internally by
-        :func:`~StreamDeck._read` to talk to the actual device.
-
-        :rtype: list(bool)
-        :return: Button states, with the origin at the top-left of the deck.
-        """
-
         states = self.device.read(1 + self.KEY_COUNT)
         if states is None:
             return None
@@ -58,36 +50,16 @@ class StreamDeckMini(StreamDeck):
         return [bool(s) for s in states]
 
     def _reset_key_stream(self):
-        """
-        Sends a blank key report to the StreamDeck, resetting the key image
-        streamer in the device. This prevents previously started partial key
-        writes that were not completed from corrupting images sent from this
-        application.
-        """
-
         payload = bytearray(self.IMAGE_REPORT_LENGTH)
         payload[0] = 0x02
         self.device.write(payload)
 
     def reset(self):
-        """
-        Resets the StreamDeck, clearing all button images and showing the
-        standby image.
-        """
-
         payload = bytearray(17)
         payload[0:2] = [0x0B, 0x63]
         self.device.write_feature(payload)
 
     def set_brightness(self, percent):
-        """
-        Sets the global screen brightness of the StreamDeck, across all the
-        physical buttons.
-
-        :param int/float percent: brightness percent, from [0-100] as an `int`,
-                                  or normalized to [0.0-1.0] as a `float`.
-        """
-
         if isinstance(percent, float):
             percent = int(100.0 * percent)
 
@@ -98,42 +70,14 @@ class StreamDeckMini(StreamDeck):
         self.device.write_feature(payload)
 
     def get_serial_number(self):
-        """
-        Gets the serial number of the attached StreamDeck.
-
-        :rtype: str
-        :return: String containing the serial number of the attached device.
-        """
-
         serial = self.device.read_feature(0x03, 17)
         return self._extract_string(serial[5:])
 
     def get_firmware_version(self):
-        """
-        Gets the firmware version of the attached StreamDeck.
-
-        :rtype: str
-        :return: String containing the firmware version of the attached device.
-        """
-
         version = self.device.read_feature(0x04, 17)
         return self._extract_string(version[5:])
 
     def set_key_image(self, key, image):
-        """
-        Sets the image of a button on the StreamDeck to the given image. The
-        image being set should be in the correct format for the device, as an
-        enumerable collection of bytes.
-
-        .. seealso:: See :func:`~StreamDeck.get_key_image_format` method for
-                     information on the image format accepted by the device.
-
-        :param int key: Index of the button whose image is to be updated.
-        :param enumerable image: Raw data of the image to set on the button.
-                                 If `None`, the key will be cleared to a black
-                                 color.
-        """
-
         if min(max(key, 0), self.KEY_COUNT) != key:
             raise IndexError("Invalid key index {}.".format(key))
 
--- src/StreamDeck/Devices/StreamDeckPedal.py
+++ src/StreamDeck/Devices/StreamDeckPedal.py
@@ -21,14 +21,6 @@ class StreamDeckPedal(StreamDeck):
     DECK_VISUAL = False
 
     def _read_key_states(self):
-        """
-        Reads the key states of the StreamDeck. This is used internally by
-        :func:`~StreamDeck._read` to talk to the actual device.
-
-        :rtype: list(bool)
-        :return: Button states, with the origin at the top-left of the deck.
-        """
-
         states = self.device.read(4 + self.KEY_COUNT)
         if states is None:
             return None
@@ -37,29 +29,12 @@ class StreamDeckPedal(StreamDeck):
         return [bool(s) for s in states]
 
     def _reset_key_stream(self):
-        """
-        Sends a blank key report to the StreamDeck, resetting the key image
-        streamer in the device. This prevents previously started partial key
-        writes that were not completed from corrupting images sent from this
-        application.
-        """
         pass
 
     def reset(self):
-        """
-        Resets the StreamDeck, clearing all button images and showing the
-        standby image.
-        """
         pass
 
     def set_brightness(self, percent):
-        """
-        Sets the global screen brightness of the StreamDeck, across all the
-        physical buttons.
-
-        :param int/float percent: brightness percent, from [0-100] as an `int`,
-                                  or normalized to [0.0-1.0] as a `float`.
-        """
         pass
 
     def get_serial_number(self):
@@ -67,28 +42,8 @@ class StreamDeckPedal(StreamDeck):
         return self._extract_string(serial[2:])
 
     def get_firmware_version(self):
-        """
-        Gets the firmware version of the attached StreamDeck.
-
-        :rtype: str
-        :return: String containing the firmware version of the attached device.
-        """
-
         version = self.device.read_feature(0x05, 32)
         return self._extract_string(version[6:])
 
     def set_key_image(self, key, image):
-        """
-        Sets the image of a button on the StreamDeck to the given image. The
-        image being set should be in the correct format for the device, as an
-        enumerable collection of bytes.
-
-        .. seealso:: See :func:`~StreamDeck.get_key_image_format` method for
-                     information on the image format accepted by the device.
-
-        :param int key: Index of the button whose image is to be updated.
-        :param enumerable image: Raw data of the image to set on the button.
-                                 If `None`, the key will be cleared to a black
-                                 color.
-        """
         pass
