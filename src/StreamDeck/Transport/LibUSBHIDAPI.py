a9f89f4b384799e2590ab9d9e050a4033ab26936
Tiago Gasiba <tiago.gasiba@gmail.com>
2024-04-07 01:41:26 +0200

    FreeBSD: use libhidapi.so

--- src/StreamDeck/Transport/LibUSBHIDAPI.py
+++ src/StreamDeck/Transport/LibUSBHIDAPI.py
@@ -154,6 +154,7 @@ class LibUSBHIDAPI(Transport):
                 "Windows": ["hidapi.dll", "libhidapi-0.dll", "./hidapi.dll"],
                 "Linux": ["libhidapi-libusb.so", "libhidapi-libusb.so.0"],
                 "Darwin": ["libhidapi.dylib"],
+                "FreeBSD": ["libhidapi.so"],
             }
 
             self.platform_name = platform.system()
