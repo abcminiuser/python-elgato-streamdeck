67dbecaa7d531ab79dd675e75ce8206f1dc1c046
Renato Schmidt <rescbr@gmail.com>
2024-08-08 19:36:36 -0300

    implements key up/down emulation

--- src/StreamDeck/Devices/Mirabox293S.py
+++ src/StreamDeck/Devices/Mirabox293S.py
@@ -26,14 +26,16 @@ class Mirabox293S(StreamDeck):
 
     DECK_TYPE = "Mirabox Stream Dock 293S"
     DECK_VISUAL = True
-    DECK_TOUCH = False # kind of...
+    DECK_TOUCH = False # kind of... it could be used for the side display.
 
     PACKET_LENGHT = 512
-    IMAGE_REPORT_HEADER_LENGTH = 16
 
     # the side display uses key ids 0x10, 0x11, 0x12 with 80x80 images.
     KEY_NUM_TO_DEVICE_KEY_ID = [0x0d, 0x0a, 0x07, 0x04, 0x01, 0xe, 0xb, 0x08, 0x05, 0x02, 0x0f, 0x0c, 0x09, 0x06, 0x03]
     KEY_DEVICE_KEY_ID_TO_NUM = {value: index for index, value in enumerate(KEY_NUM_TO_DEVICE_KEY_ID)}
+
+    # see note in _read_control_states() method.
+    _key_triggered_last_read = False
 
     # 72 x 72 black JPEG
     BLANK_KEY_IMAGE = [
@@ -92,25 +94,35 @@ class Mirabox293S(StreamDeck):
         return payload
 
     def _read_control_states(self):
-        device_input_data = self.device.read(self.PACKET_LENGHT)
-        if device_input_data is None:
-            return None
-        
-        if(device_input_data.startswith(bytes([0x41, 0x43, 0x4b, 0x00, 0x00, 0x4f, 0x4b, 0x00]))): # ACK\0\0OK\0
-            triggered_key = self._convert_device_key_id_to_key_num(int.from_bytes(device_input_data[9:10], 'big', signed=False))            
-        else:
-            # we don't know how to handle the response
-            return None
-        
         states = [False] * self.KEY_COUNT
-        states[triggered_key] = True
+
+        # _key_triggered_last_read exists since 293S only triggers an HID event when a button is released.
+        # there are no key down and key up events, so we have to simulate the key being pressed and released.
+        # if a firmware upgrade that supports key down/up events is released, this variable can be removed from the code.
+
+        if not self._key_triggered_last_read:
+            device_input_data = self.device.read(self.PACKET_LENGHT)
+            if device_input_data is None:
+                return None
+            
+            if(device_input_data.startswith(bytes([0x41, 0x43, 0x4b, 0x00, 0x00, 0x4f, 0x4b, 0x00]))): # ACK\0\0OK\0
+                triggered_key = self._convert_device_key_id_to_key_num(int.from_bytes(device_input_data[9:10], 'big', signed=False))            
+            else:
+                # we don't know how to handle the response
+                return None
+            
+            states = [False] * self.KEY_COUNT
+            states[triggered_key] = True
+            self._key_triggered_last_read = True
+        else:
+            self._key_triggered_last_read = False
 
         return {
             ControlType.KEY: states
         }
 
     def _reset_key_stream(self):
-        return
+        self.reset()
 
     def reset(self):
         # disconnect # CRT\0\0DIS
