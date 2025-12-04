47c97ad508609cfc7312df556fb82a263533bd81
Dean Camera <dean@fourwalledcubicle.com>
2025-12-04 18:08:58 +1100

    Bump minimum Python version to 3.10.

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
    long_description=long_description,
    long_description_content_type="text/markdown",
    include_package_data=True,
-   python_requires='>=3.9',
+   python_requires='>=3.10',
 )
